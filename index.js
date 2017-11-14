c0cde21cd018d3e4c5609e10020575271c461db7
Charlie <charlie@digitpaper.com>
2017-11-13 20:16:16 -0500

    tweaks

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 (function(){
 
     $(window).ready(function(){
-        
+        $("#athBtn").click(function(){
+        	$("#popOver").popover();
+        });
+
+        $('[data-toggle="popover"]').popover();
     });
 })();
