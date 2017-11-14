6c4f7e61aa346fe15b59833ef259aebe47cb052d
Charlie <charlie@digitpaper.com>
2017-11-13 20:54:46 -0500

    fix

--- index.js
+++ index.js
@@ -2,15 +2,6 @@
 (function(){
 
     $(window).ready(function(){
-        $('#athBtn').popover({
-        	title: "Add to Home Screen", 
-        	content: '1. Click <img src="img/ios-share.svg" width="20px" height="20px"> "Share" <br>2. Click "Add to Home Screen"',
-        	html: true, 
-        	placement: "top"
-        });
-
-        $("#athBtn").click(function(){
-			//athSpan.popover("show");
-        });
+        $('#athBtn').popover();
     });
 })();
