187a57e73107da0be9873aaa7a991e5465c06616
digitpaper <charlie@digitpaper.com>
2017-11-14 15:47:19 -0500

    add xyz

--- index.js
+++ index.js
@@ -2,25 +2,36 @@
 
 (function(){
 
-    $(window).ready(function(){
-    	$('#athBtn').click(function(){
-        	$('#athBtn').popover('toggle');
-        });
-
-        window.ondevicemotion = function(event) {
-          ax = event.accelerationIncludingGravity.x
-          ay = event.accelerationIncludingGravity.y
-          az = event.accelerationIncludingGravity.z
-          rotation = event.rotationRate;
-          if (rotation != null) {
-            arAlpha = Math.round(rotation.alpha);
-            arBeta = Math.round(rotation.beta);
-            arGamma = Math.round(rotation.gamma);
-          }
-          $("#alpha").html(arAlpha);
-        }
-
-
-
+  $(window).ready(function(){
+    $('#athBtn').click(function(){
+      $('#athBtn').popover('toggle');
     });
+
+    window.ondevicemotion = function(event) {
+      ax = event.accelerationIncludingGravity.x
+      ay = event.accelerationIncludingGravity.y
+      az = event.accelerationIncludingGravity.z
+      rotation = event.rotationRate;
+      if (rotation != null) {
+        arAlpha = Math.round(rotation.alpha);
+        arBeta = Math.round(rotation.beta);
+        arGamma = Math.round(rotation.gamma);
+        $("#arAlpha").html(alpha);
+        $("#arBeta").html(beta);
+        $("#arGamma").html(gamma);
+      }          
+    }
+
+    window.ondeviceorientation = function(event) {
+      alpha = Math.round(event.alpha);
+      beta = Math.round(event.beta);
+      gamma = Math.round(event.gamma);
+      $("#alpha").html(alpha);
+      $("#beta").html(beta);
+      $("#gamma").html(gamma);
+    }
+
+
+
+  });
 })();
