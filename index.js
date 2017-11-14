d204e968cba3d2d0e083e2b7133ac5f55c8f604a
digitpaper <charlie@digitpaper.com>
2017-11-14 15:17:31 -0500

    add alpha

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 
+
 (function(){
 
     $(window).ready(function(){
     	$('#athBtn').click(function(){
         	$('#athBtn').popover('toggle');
         });
+
+        window.ondevicemotion = function(event) {
+          ax = event.accelerationIncludingGravity.x
+          ay = event.accelerationIncludingGravity.y
+          az = event.accelerationIncludingGravity.z
+          rotation = event.rotationRate;
+          if (rotation != null) {
+            arAlpha = Math.round(rotation.alpha);
+            arBeta = Math.round(rotation.beta);
+            arGamma = Math.round(rotation.gamma);
+          }
+          $("#alpha").html(arAlpha);
+        }
+
+
+
     });
 })();
