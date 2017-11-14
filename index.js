6b7050385f91ed00339ad5ba653a3df68ae1bdfe
digitpaper <charlie@digitpaper.com>
2017-11-14 16:28:54 -0500

    add fly

--- index.js
+++ index.js
@@ -29,9 +29,50 @@
       $("#alpha").html(alpha);
       $("#beta").html(beta);
       $("#gamma").html(gamma);
+
+      fly(beta, gamma);
+
+    }
+  });
+
+  function fly(beta, gamma){
+    var p = $("#airPlane").position();
+    var t = p.top;
+    var l = p.left;
+
+    if (gamma > 0) {
+      l ++;
+    } else if (gamma < 0) {
+      l --;
+    } else {
+      l = l;
     }
 
+    if (l > $( window ).width()) {
+      l = $( window ).width();
+    } 
 
+    if (l < 0) {
+      l = 0;
+    }
 
-  });
+    if (beta > 0) {
+      t ++;
+    } else if (beta < 0) {
+      t --;
+    } else {
+      t = t;
+    }
+
+    if (t > $( window ).height()) {
+      t = $( window ).height();
+    } 
+
+    if (t < 0) {
+      t = 0;
+    }
+
+    $("#airPlane").css('position: absolute; top: '+ t +'px; left: '+ l +'px;');
+  }
+
 })();
