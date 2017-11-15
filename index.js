5b71e10ddf1014e06c4c78edc4290f34cf468e4f
Charlie <charlie@digitpaper.com>
2017-11-14 22:12:23 -0500

    add enemy

--- index.js
+++ index.js
@@ -7,6 +7,9 @@
   var speedY = 0;
   var reach = 200;
 
+  var turnT = false;
+  var turnL = false;
+
   var windowW = 0;
   var windowH = 0;
   
@@ -16,6 +19,8 @@
     windowW = $(window).width();
     windowH = $(window).height();
     $("#airPlane").css({'position': 'absolute', 'top': (windowH/2) +'px', 'left': (windowW/2) +'px'});
+    $("#enemyPlane").css({'position': 'absolute', 'top': '60px', 'left': (windowW/2) +'px'});
+
 
     window.ondevicemotion = function(event) {
       ax = event.accelerationIncludingGravity.x
@@ -42,11 +47,68 @@
       fly(beta, gamma); 
     }, 10);
 
+    setInterval(function(){
+      enemyFly(turnT, turnL); 
+    }, 20);
+
+    setInterval(function(){
+      var diceT = Math.floor(Math.random() * 10);
+      var diceL = Math.floor(Math.random() * 10);
+      if (diceT > 4) {
+        turnT = true;
+      } else {
+        turnT = false;
+      }
+
+      if (diceL > 4) {
+        diceL = true;
+      } else {
+        diceL = false;
+      }
+    }, 500);
+
     $('body').click(function(){
       lauchMissle();
     });
 
   });
+
+  function enemyFly(turnT, turnL){
+
+    var p = $("#enemyPlane").position();
+    var t = p.top;
+    var l = p.left;    
+
+    if (turnT) {
+      t--;
+    } else {
+      t++;
+    }
+
+    if (t > windowH) {
+      t = 0;
+    } 
+
+    if (t < 0) {
+      t = windowH;
+    }
+
+    if (turnL) {
+      l--;
+    } else {
+      l++;
+    }
+
+    if (l > windowW) {
+      l = 0;
+    } 
+
+    if (l < 0) {
+      l = windowW;
+    }
+
+    $("#enemyPlane").css({'position': 'absolute', 'top': t +'px', 'left': l +'px'});
+  }
 
   function fly(beta, gamma){
 
