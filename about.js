4e358f5304d7eeb843e505881057ac23d0ec812c
Charlie <charlie@digitpaper.com>
2017-11-18 14:14:53 -0500

    add more

--- about.js
+++ about.js
@@ -3,9 +3,40 @@
     $('#athBtn').click(function(){
       $('#athBtn').popover('toggle');
     });
+
+	var action = '';
+
+
+
   });
 
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+
 function post_data(url, data, success_handler, error_handler) {
+
+	/*
+var success_handler = fucntion(){};
+    var error_handler = fucntion(){};
+    var url = '';
+    var data = {
+
+    };
+    post_data(url);
+	*/
     $.ajax({
       url: url,
       type: 'POST',
