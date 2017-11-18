41d4cf3822156da3252b94160991b3edb88c828c
Charlie <charlie@digitpaper.com>
2017-11-18 13:59:32 -0500

    add ajax post function

--- about.js
+++ about.js
@@ -4,4 +4,21 @@
       $('#athBtn').popover('toggle');
     });
   });
+
+function post_data(url, data, success_handler, error_handler) {
+    $.ajax({
+      url: url,
+      type: 'POST',
+      contentType: 'application/json; charset=UTF-8',
+      dataType: 'json',
+      data: JSON.stringify(data),
+      success: function(response) {
+        success_handler(response);
+      },
+      error: function() {
+        error_handler();
+      }
+    });
+  }
 })();
+
