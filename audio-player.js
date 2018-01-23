7bb717e6ffebbff8d81139cc981dff1f9e6967af
digitpaper <charlie@digitpaper.com>
2018-01-23 16:30:05 -0500

    Update audio-player.js

--- audio-player.js
+++ audio-player.js
@@ -20,13 +20,14 @@
 function updateSrc(sourceUrl) {
     var audio = $("#player");      
     $("#player-src").attr("src", sourceUrl);
-    /****************/
+    
+    /*
     audio[0].pause();
     audio[0].load();//suspends and restores all audio element
 
     //audio[0].play(); changed based on Sprachprofi's comment below
     audio[0].oncanplaythrough = audio[0].play();
-    /****************/
+    */
 }
 
 
