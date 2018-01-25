f072fc290b085cfd66dd771574e4dbdf6f8f5941
Charlie-Wellesley <cgao@wellesley.edu>
2018-01-25 15:02:04 -0500

    customizing player

--- audio-player.js
+++ audio-player.js
@@ -6,31 +6,36 @@
 (function(){
 
   var audioURL = '';
+  var playTime = 0;
 
   $(window).ready(function(){
     audioURL = getUrlParameter('src'); 
-    
-    /*
-    audiojs.events.ready(function() {
-     var as = audiojs.createAll();
-    });
-    */
-    
     updateSrc(audioURL);
   });
 
 
 function updateSrc(sourceUrl) {
-    var audio = $("#player");      
+    
     $("#player-src").attr("src", sourceUrl);
     
-    
+    var audio = $("#player");
     audio[0].pause();
     audio[0].load();//suspends and restores all audio element
 
+    var savedTime = getCookie("src" + sourceUrl);
+    if (savedTime) {
+      audio[0].currentTime = savedTime;
+      playTime = savedTime;
+      console.log('[Resume] ' + savedTime);
+    } else {
+      var saveTimeName = 'src' + sourceUrl;
+      var savedTime = 0;
+      setCookie(saveTimeName, savedTime, 7);
+      console.log('[New Play] ' + savedTime);
+    }
+
     //audio[0].play(); changed based on Sprachprofi's comment below
-    audio[0].oncanplaythrough = audio[0].play();
-    
+    audio[0].oncanplaythrough = audio[0].play();    
 }
 
 
@@ -40,6 +45,30 @@ function getUrlParameter(name) {
   var results = regex.exec(location.search);
   return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 };
+
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 
 
 })();
