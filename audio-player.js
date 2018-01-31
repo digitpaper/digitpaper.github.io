78e21211022f73134e03c9c839b05f01487de4c3
Charlie-Wellesley <cgao@wellesley.edu>
2018-01-31 15:47:01 -0500

    add local storage

--- audio-player.js
+++ audio-player.js
@@ -21,23 +21,13 @@ function updateSrcAndPlay(sourceUrl) {
     audio[0].pause();
     audio[0].load();//suspends and restores all audio element
 
-    var savedTime = getCookie("src" + sourceUrl);
+    var savedTime = localStorage.getItem("src-" + sourceUrl);
 
-    audio[0].oncanplaythrough = audio[0].play();
-
-    if (savedTime) {    
-      var r = confirm('Resume last track?');
-      if (r) {
-        audio[0].currentTime = savedTime;
-        console.log('[Resume] ' + savedTime);
-      }
-    } else {
-      var saveTimeName = 'src' + sourceUrl;
-      var savedTime = 0;
-      setCookie(saveTimeName, savedTime, 7);
-      console.log('[New Play] ' + savedTime);
+    if (savedTime) {
+      audio[0].currentTime = savedTime;
     }
 
+    audio[0].oncanplaythrough = audio[0].play();
     updateTimer(sourceUrl, audio[0]);
 }
 
@@ -49,35 +39,11 @@ function getUrlParameter(name) {
   return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 };
 
-
-function setCookie(cname,cvalue,exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
 function updateTimer(src, player){
-  var saveTimeName = 'src' + src;  
+  var saveTimeName = 'src-' + src;  
   setInterval(function(){
     var currentTime = player.currentTime;
-    setCookie(saveTimeName,currentTime,7)
+    localStorage.setItem(saveTimeName, currentTime)
   }, 3000); // Every 3 seconds
 }
 
